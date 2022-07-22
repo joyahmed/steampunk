08c9e92c4d11fdf5613f27373a7ccf51fac74f24
joyahmed <joy_ahmed_007@yahoo.com>
2022-07-22 08:22:52 +0600

    ceated player and game object

--- index.js
+++ index.js
@@ -2,34 +2,45 @@ onload = () => {
 	const canvas = document.getElementById('canvas1');
 	const ctx = canvas.getContext('2d');
 	canvas.width = 1500;
-  canvas.height = 500;
+	canvas.height = 500;
 
-  class InputHandler {
+	class InputHandler {}
 
-  }
-
-  class Projectile {
-
-  }
-  class Particle {
-
-  }
-  class Player {
-
-  }
-  class Enemy {
-
-  }
-  class Layer {
-
-  }
-  class Background {
-
-  }
-  class UI {
-
-  }
+	class Projectile {}
+	class Particle {}
+	class Player {
+		constructor(game) {
+      this.game = game;
+      this.width = 120;
+      this.height = 190;
+      this.x = 20;
+      this.y = 100;
+      this.speedY = 0;
+    }
+    update () {
+      this.y += this.speedY
+    }
+    draw (context) {
+      context.fillRect(this.x, this.y, this.width, this.height)
+    }
+	}
+	class Enemy {}
+	class Layer {}
+	class Background {}
+	class UI {}
   class Game {
-    
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this);
+    }
+    update () {
+      this.player.update();
+    }
+    draw (context) {
+      this.player.draw(context);
+    }
   }
+
+  const game = new Game(canvas.width, canvas.height);
 };
