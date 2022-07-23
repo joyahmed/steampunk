33c40d8c0327d59e59f204a366d5742a32d8f995
joyahmed <joy_ahmed_007@yahoo.com>
2022-07-24 03:31:41 +0600

    ✅game timer

--- index.js
+++ index.js
@@ -141,6 +141,9 @@ onload = () => {
 			for (let i = 0; i < this.game.ammo; i++) {
 				context.fillRect(20 + 5 * i, 50, 3, 20);
 			}
+			//timer
+			const formattedTime = (this.game.gameTime * 0.001).toFixed(1);
+			context.fillText('Timer: ' + formattedTime, 20, 100);
 			// game over messages
 			if (this.game.gameOver) {
 				context.textAlign = 'center';
@@ -187,8 +190,12 @@ onload = () => {
 			this.gameOver = false;
 			this.score = 0;
 			this.winningScore = 10;
+			this.gameTime = 0;
+			this.timeLimit = 5000;
 		}
 		update(deltaTime) {
+			if (!this.gameOver) this.gameTime += deltaTime;
+			if (this.gameTime > this.timeLimit) this.gameOver = true;
 			this.player.update();
 			if (this.ammoTimer > this.ammoInterval) {
 				if (this.ammo < this.maxAmmo) this.ammo++;
@@ -207,7 +214,7 @@ onload = () => {
 						projectile.markedForDeletion = true;
 						if (enemy.lives <= 0) {
 							enemy.markedForDeletion = true;
-							this.score += enemy.score;
+							if (!this.gameOver) this.score += enemy.score;
 							if (this.score > this.winningScore)
 								this.gameOver = true;
 						}
